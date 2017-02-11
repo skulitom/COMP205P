6f5e3f7f3143df1ac24d42ddcecf48e464548ff0
skulitom <artemskulimovskiy@gmail.com>
2017-02-11 11:46:38 +0000

    commit 1

---
+++ App1/App1/Pages/LoginXaml.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1.Pages
+{
+    public partial class LoginXaml : ContentPage
+    {
+        public LoginXaml()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NavigationXaml());
+        }
+    }
+}
